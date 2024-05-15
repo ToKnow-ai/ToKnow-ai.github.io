b2bae219669a3ed257f564893b26af6eb0cc2850
Kabue Charles <mckabue@gmail.com>
2024-05-16 01:54:09 +0300

    youtube image

--- filters/replace_base64_video_src.lua
+++ filters/replace_base64_video_src.lua
@@ -2,13 +2,46 @@ local str_ends_with = require "utils.str_ends_with"
 local notebook_special_comments_walker = require "utils.notebook_special_comments_walker"
 local is_output_cell = require "utils.is_output_cell"
 
-local function makeBox(text, url, icon, color) 
-  return pandoc.Div(pandoc.List({
-    pandoc.RawInline('latex', '\\begin{centering}\\begin{tcolorbox}[hbox,\ncolframe=lightgray,\ncolback=white]\n'),
-    pandoc.RawInline('latex', '\\textcolor{' .. color .. '}{{\\Large {' .. icon .. '}}} %'),
-    pandoc.Link(pandoc.RawInline("latex", '\\raisebox{0.1 em}{' .. text .. '}'),url),
-    pandoc.RawInline('latex', '\n\\end{tcolorbox}\\end{centering}\n\\vspace{1em}')
-  }))
+---get_youtube_image
+---@param video_src string
+---@return string|nil
+local get_youtube_image = function(video_src)
+  local video_id = 
+    -- "www.youtube.com/watch?v=kCc8FmEb1nY"
+    string.match(video_src, ".+youtube.com/watch%?v%=(.+)$") or
+    -- "https://youtu.be/kCc8FmEb1nY?si=77OsaKikOnJgRRRy"
+    string.match(video_src, ".+youtu.be/(.+)%?.+$") or
+    -- "https://www.youtube.com/embed/kCc8FmEb1nY?si=XspSx2xnp7xhYgDe"
+    string.match(video_src, ".+youtube.com/embed/(.+)%?.+$")
+  if video_id then
+    return 'https://img.youtube.com/vi/' .. video_id .. '/hqdefault.jpg'
+  end
+  return nil
+end
+
+local makeBox = function (text, url, icon, color)
+  local youtube_image = get_youtube_image(url)
+  if not youtube_image then
+    return pandoc.Div(pandoc.List({
+      pandoc.RawInline('latex', '\\begin{centering}\\begin{tcolorbox}[hbox,\ncolframe=lightgray,\ncolback=white]\n'),
+      pandoc.RawInline('latex', '\\textcolor{' .. color .. '}{{\\Large {' .. icon .. '}}} %'),
+      pandoc.Link(pandoc.RawInline("latex", '\\raisebox{0.1 em}{' .. text .. '}'), url),
+      pandoc.RawInline('latex', '\n\\end{tcolorbox}\\end{centering}\n\\vspace{1em}')
+    }))
+  else
+    local test_image = '/home/anon-b/Downloads/hqdefault.jpg'
+    local blocks = pandoc.RawInline(
+      'latex',
+      '\\href{' .. url .. '}{ \
+         \\begin{centering} \
+            \\begin{tcolorbox}[hbox,  colframe=' .. color .. ', colback=white] \
+              \\includegraphics[width=0.5\\textwidth]{' .. test_image .. ' } \
+            \\end{tcolorbox}\
+        \\end{centering}\
+        \\vspace{1em} \
+      }')
+    return pandoc.Div(blocks)
+  end
 end
 
 -- Function to return video block for PDF
