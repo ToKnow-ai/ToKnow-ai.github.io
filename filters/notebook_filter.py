ab8aec2667f50928bd0831578c0ef65f20206958
Kabue Charles <mckabue@gmail.com>
2024-05-26 12:03:58 +0300

    filter

--- filters/notebook_filter.py
+++ filters/notebook_filter.py
@@ -18,11 +18,13 @@ def extract_quarto_metadata(cells: list[NotebookNode]) -> list[NotebookNode]:
                     if len(splits) > 2:
                         meatadata_value = splits[-1]
                         if len(meatadata_value) > 0 and len(meatadata_value.strip()) > 0:
-                          metadata[metadata_key] = meatadata_value.strip()
+                          # double space spoils the index page!
+                          metadata[metadata_key] = meatadata_value.strip().replace("\n\n", "\n")
                           continue
         new_cells.append(cell)
     if any(metadata):
-        metadata_cell = v4.new_markdown_cell(source=f'---\n{yaml.dump(metadata)}\n---')
+        source = yaml.dump(metadata)
+        metadata_cell = v4.new_markdown_cell(source=f'---\n{source}\n---')
         new_cells.insert(0, metadata_cell)
     return new_cells
 
