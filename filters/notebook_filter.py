8766a62c747d854ec03a7f7cc21b554206851f86
Kabue Charles <mckabue@gmail.com>
2024-05-26 14:04:35 +0300

    formating markdown pretty well now

--- filters/notebook_filter.py
+++ filters/notebook_filter.py
@@ -2,6 +2,7 @@ import sys
 from nbformat import NotebookNode, v4, write as nb_write
 import re
 import yaml
+import strip_markdown
 
 def remove_lists(markdown_text):
     # Remove ordered lists
@@ -17,21 +18,20 @@ def remove_lists(markdown_text):
 def extract_quarto_metadata(cells: list[NotebookNode]) -> list[NotebookNode]:
     new_cells = []
     metadata = {}
-    pattern = r"<!--\s*metadata:\s*(.*?)\s*-->"
+    pattern = r"<!--\s*metadata:\s*(.*?)\s*-->\s*(.*?)\s*(?=<!-- metadata:|$)"
 
     for cell in cells:
         if cell and cell.cell_type == 'markdown':
-            match = re.search(pattern, cell.source, re.IGNORECASE)
-            if match:
-                metadata_key = match.group(1)
-                if metadata_key:
-                    splits = re.split(pattern, cell.source, re.IGNORECASE)
-                    if len(splits) > 2:
-                        meatadata_value = splits[-1]
-                        if len(meatadata_value) > 0 and len(meatadata_value.strip()) > 0:
-                          # html list in listings page spoils UI.
-                          metadata[metadata_key] = remove_lists(meatadata_value.strip().replace("\n\n", "\n"))
-                          continue
+            matches = re.findall(pattern, cell.source, re.DOTALL)
+            skip_this_cell = False
+            for (metadata_key, meatadata_value) in matches:
+                meatadata_value = meatadata_value.strip()
+                if len(metadata_key) > 0 and len(meatadata_value) > 0:
+                    # html list in listings page spoils UI.
+                    metadata[metadata_key] = remove_lists(strip_markdown.strip_markdown(meatadata_value.replace("\n\n", "\n")))
+                    skip_this_cell = True
+            if skip_this_cell:
+                continue
         new_cells.append(cell)
     if any(metadata):
         source = yaml.dump(metadata)
