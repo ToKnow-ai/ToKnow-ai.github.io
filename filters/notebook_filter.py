a9a27707d7815d9a591daac77032678c1acc7ce7
Kabue Charles <mckabue@gmail.com>
2024-05-26 10:33:44 +0300

    yaml

--- filters/notebook_filter.py
+++ filters/notebook_filter.py
@@ -1,14 +1,35 @@
 import sys
-import nbformat
-from nbformat import NotebookNode
+from nbformat import NotebookNode, v4, write as nb_write
+import re
+import yaml
+
+def extract_quarto_metadata(cells: list[NotebookNode]) -> list[NotebookNode]:
+    new_cells = []
+    metadata = {}
+    pattern = r"<!--\s*metadata:\s*(.*?)\s*-->"
+
+    for cell in cells:
+        if cell and cell.cell_type == 'markdown':
+            match = re.search(pattern, cell.source, re.IGNORECASE)
+            if match:
+                metadata_key = match.group(1)
+                if metadata_key:
+                    splits = re.split(pattern, cell.source, re.IGNORECASE)
+                    if len(splits) > 2:
+                        meatadata_value = splits[-1]
+                        if len(meatadata_value) > 0 and len(meatadata_value.strip()) > 0:
+                          metadata[metadata_key] = meatadata_value.strip()
+                          continue
+        new_cells.append(cell)
+    if any(metadata):
+        metadata_cell = v4.new_markdown_cell(source=f'---\n{yaml.dump(metadata)}\n---')
+        new_cells.insert(0, metadata_cell)
+    return new_cells
 
 # read notebook from stdin
-nb: NotebookNode = nbformat.reads(sys.stdin.read(), as_version = 4) as NotebookNode
+notebook = v4.reads(sys.stdin.read())
 
-# prepend a comment to the source of each cell
-for index, cell in enumerate(nb.cells):
-  if cell.cell_type == 'code':
-     cell.source = "# comment\n" + cell.source
-  
+notebook.cells = extract_quarto_metadata(notebook.cells)
+
 # write notebook to stdout 
-nbformat.write(nb, sys.stdout)
+nb_write(notebook, sys.stdout)
--- filters/print.lua
+++ filters/print.lua
@@ -1,25 +1,8 @@
 return {
   ---@param doc pandoc.Pandoc
+  ---@return pandoc.Pandoc
   Pandoc = function (doc)
-    quarto.log.debug('Pandoc.doc', doc)
-
-    local blocks = doc.blocks:walk {
-      ---@param float pandoc.FloatRefTarget
-      ---@return pandoc.FloatRefTarget
-      FloatRefTarget = function(float)
-        -- if float.type == 'Listing' then
-          quarto.log.debug(float)
-          return float
-        -- end
-      end
-    }
-    
-    return pandoc.Pandoc(blocks, doc.meta)
-  end,
-  FloatRefTarget = function(float)
-    -- if float.type == 'Listing' then
-      quarto.log.debug(float)
-      return float
-    -- end
+    quarto.log.debug(doc)
+    return doc
   end
 }
