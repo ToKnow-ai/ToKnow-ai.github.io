a020b3eca068e884b500be5c5988428724af2b03
Kabue Charles <mckabue@gmail.com>
2024-05-28 08:39:14 +0300

    fund managers

--- utils/web_screenshot.py
+++ utils/web_screenshot.py
@@ -1,20 +1,18 @@
-import asyncio
 from typing import Callable
 from pyppeteer import launch
 from pyppeteer.page import Page
 from PIL import Image
 import io
-import nest_asyncio
 import shutil
 import inspect
-
-nest_asyncio.apply()
 
 async def web_screenshot_async(
         url: str, 
         *, 
         action: Callable[[Page], None] = None, 
-        executable_path: str = None) -> Image.Image:
+        executable_path: str = None,
+        width: int = 0,
+        height: int = 0) -> Image.Image:
     executable_path = executable_path or\
                     shutil.which("google-chrome") or\
                     shutil.which("chromium-browser") or\
@@ -24,9 +22,7 @@ async def web_screenshot_async(
     # Open a new page
     page = await browser.newPage()
 
-    # Set viewport width and height
-    width = 1600  # Specify your desired width
-    height = 0  # Setting height to 0 makes it full height
+    # 0 makes it default height or default width
     await page.setViewport({'width': width, 'height': height})
     
     # Go to the specified URL
@@ -43,13 +39,3 @@ async def web_screenshot_async(
     # Convert the bytes to a PIL Image
     image = Image.open(io.BytesIO(screenshot_bytes))
     return image
-
-def web_screenshot(
-        url: str, 
-        *, 
-        action: Callable[[Page], None] = None, 
-        executable_path: str = None) -> Image.Image:
-    return asyncio.run(web_screenshot_async(
-        url, 
-        action = action, 
-        executable_path = executable_path))
