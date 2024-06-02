ddfcb378917e538e5e76540a3440fe10b4da4c4b
Kabue Charles <mckabue@gmail.com>
2024-06-02 16:04:10 +0300

    table

--- utils/get_browser.py
+++ utils/get_browser.py
@@ -3,24 +3,26 @@ import shutil
 from pyppeteer.page import Page
 from pyppeteer.browser import Browser
 
-async def get_browser_async(executable_path: str = None) -> Browser:
+async def get_browser_async(executable_path: str = None, *, headless = True, incognito = False) -> Browser:
     executable_path = executable_path or\
                     shutil.which("google-chrome") or\
                     shutil.which("chromium-browser") or\
                     shutil.which("chromium")
     # Launch a new browser instance
     browser = await launch({
-        'headless': True, 
+        'headless': headless, 
         'executablePath': executable_path,
-        'args': ["--disable-web-security"],
+        'args': ["--disable-web-security"] + (['--incognito'] if incognito else []),
         'ignoreHTTPSErrors': True })
     return browser
 
 async def get_browser_page_async(
         executable_path: str = None,
         width: int = 0,
-        height: int = 0) -> tuple[Page, Browser]:
-    browser = await get_browser_async(executable_path)
+        height: int = 0,
+        *,
+        headless: bool = True) -> tuple[Page, Browser]:
+    browser = await get_browser_async(executable_path, headless=headless)
     
     # Open a new page
     page = await browser.newPage()
