460cc3882e5a707943351ac244a5234ac1337883
Kabue Charles <mckabue@gmail.com>
2024-09-14 02:24:03 +0300

    Enhance web screenshot functionality and update analysis notebook
    - Added cropping options to the web_screenshot_async function.
    - Refactored screenshot calls in the analysis notebook to utilize new cropping feature.
    - Improved data merging logic for collective units in the notebook.

--- python_utils/web_screenshot.py
+++ python_utils/web_screenshot.py
@@ -1,6 +1,6 @@
 import inspect
 import io
-from typing import Callable
+from typing import Callable, Literal
 from PIL import Image
 from pyppeteer.page import Page, ElementHandle
 from .get_browser import get_browser_page_async
@@ -13,7 +13,8 @@ async def web_screenshot_async(
         width: int = 0,
         height: int = 0,
         # pyppeteer.page.Page.screenshot options
-        options: dict = {'fullPage': True }) -> Image.Image:
+        options: dict = {'fullPage': True },
+        crop: dict[Literal['left', 'top', 'right', 'bottom'], int | None] = { 'left': None, 'top': None, 'right': None, 'bottom': None }) -> Image.Image:
     
     page, browser = await get_browser_page_async(executable_path, width = width, height = height)
     
@@ -31,4 +32,9 @@ async def web_screenshot_async(
     await browser.close()
     # Convert the bytes to a PIL Image
     image = Image.open(io.BytesIO(screenshot_bytes))
+    image = image.crop((
+        crop.get('left', 0),
+        crop.get('top', 0),
+        crop.get('right', image.size[0]),
+        crop.get('bottom', image.size[1])))
     return image
