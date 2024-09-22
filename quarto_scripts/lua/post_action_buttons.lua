cea15dcf5e393d657a8bb5997e9957a5da9f1002
Kabue Charles <mckabue@gmail.com>
2024-09-22 11:52:13 +0300

    Update .gitignore and _quarto.yml for improved file management; refactor post action buttons for Markdown support

--- quarto_scripts/lua/post_action_buttons.lua
+++ quarto_scripts/lua/post_action_buttons.lua
@@ -17,81 +17,59 @@ local function urlencode(str)
   return str
 end
 
+local link_button_classes = "{.btn .btn-outline-success .btn-sm}"
+
 -- Function to create a Google Colab link
----@param ipynb_output_uri string
----@param title string
----@param badge_url string
+---@param ipynb_uri string
 ---@return string
-local function create_colab_link(ipynb_output_uri, title, badge_url)
-  return 
-    '<a \
-      target="_blank" \
-      href="https://colab.research.google.com/#fileId=' .. ipynb_output_uri .. '" \
-      aria-label="' .. title .. '" \
-      title="' .. title ..'">\
-      <img src="' .. badge_url .. '" aria-label="' .. title .. '" title="' .. title .. '" />\
-    </a>'
+local function create_colab_markdown(ipynb_uri)
+  local title = "Open in Colab"
+  local colab_url = 'https://colab.research.google.com/#fileId=' .. urlencode(ipynb_uri)
+  return '[{{< fa code >}} ' .. title .. '](' .. colab_url .. ')' .. link_button_classes
 end
 
 -- Function to create a Download link for the notebook
----@param ipynb_output_uri string
----@param title string
----@param badge_url string
+---@param ipynb_uri string
 ---@return string
-local function create_download_link(ipynb_output_uri, title, badge_url)
-  return 
-      '<a \
-      target="_blank" \
-      style="height:30px" \
-      href="' .. ipynb_output_uri .. '" \
-      aria-label="' .. title .. '" \
-      title="' .. title .. '">\
-      <img src="' .. badge_url .. '" aria-label="' .. title .. '" title="' .. title .. '" />\
-    </a>'
+local function create_download_markdown(ipynb_uri)
+  local title = "Download Notebook"
+  return '[{{< fa download >}} ' .. title .. '](' .. ipynb_uri .. ')' .. link_button_classes
 end
 
 -- Function to create a Deepnote link
----@param ipynb_output_uri string
----@param title string
----@param badge_url string
+---@param ipynb_uri string
 ---@return string
-local function create_deepnote_link(ipynb_output_uri, title, badge_url)
-  return 
-      '<a \
-      target="_blank" \
-      href="https://deepnote.com/launch?url=' .. urlencode(ipynb_output_uri) .. '" \
-      aria-label="' .. title .. '" \
-      title="' .. title .. '">\
-      <img src="' .. badge_url .. '" aria-label="' .. title .. '" title="' .. title .. '" />\
-    </a>'
+local function create_deepnote_markdown(ipynb_uri)
+  local title = "Open in Deepnote"
+  local deepnote_url = 'https://deepnote.com/launch?url=' .. urlencode(ipynb_uri)
+  return '[{{< fa code >}} ' .. title .. '](' .. deepnote_url .. ')' .. link_button_classes
 end
 
 -- Function to create a PDF link
----@param pdf_output_uri string
----@param title string
----@param badge_url string
+---@param pdf_uri string
 ---@return string
-local function create_PDF_link(pdf_output_uri, title, badge_url)
-  return 
-    '<a \
-      target="_blank" \
-      href="' .. pdf_output_uri .. '" \
-      aria-label="' .. title .. '" \
-      title="' .. title ..'">\
-      <img src="' .. badge_url .. '" aria-label="' .. title .. '" title="' .. title .. '" />\
-    </a>'
+local function create_pdf_markdown(pdf_uri)
+  local title = "Download as PDF"
+  return '[{{< fa file-pdf >}} ' .. title .. '](' .. pdf_uri .. ')' .. link_button_classes
 end
 
 -- buttons_wrapper
----@param html string
----@return string
-local buttons_wrapper = function (html)
-  local links_html = 
-    '<div class="d-flex justify-content-center gap-3 align-items-center flex-wrap clearfix p-1 post_action_buttons">'
-      .. html ..
-    '</div>'
-  links_html = '<hr class="mt-1 mb-1 w-50 mx-auto"/>' .. links_html .. '<hr class="mt-1 mb-1 w-50 mx-auto mb-5"/>'
-  return links_html
+---@param markdown_text string
+---@return pandoc.List
+local create_link_wrapper = function (markdown_text)
+  local html_classes =
+  "d-flex justify-content-center gap-3 align-items-center flex-wrap clearfix p-1 post_action_buttons"
+  local blocks = pandoc.List:new {}
+  blocks:insert(pandoc.RawInline('latex', '\\begin{centering}'))
+  blocks:insert(pandoc.RawInline('latex', '\\vspace{-1em} \\rule{0.5\\linewidth}{0.5pt}'))
+  blocks:insert(pandoc.RawInline('html', '<hr class="mt-1 mb-1 w-50 mx-auto" />'))
+  -- blocks:insert(pandoc.RawInline('html', '<div class="' .. html_classes  .. '">'))
+  blocks:extend(quarto.utils.string_to_blocks('<div class="' .. html_classes .. '">' .. markdown_text .. '</div>'))
+  -- blocks:insert(pandoc.RawInline('html', '</div>'))
+  blocks:insert(pandoc.RawInline('html', '<hr class="mt-1 mb-1 w-50 mx-auto mb-5" />'))
+  blocks:insert(pandoc.RawInline('latex', '\\vspace{-0.5em}  \\rule{0.5\\linewidth}{0.5pt}'))
+  blocks:insert(pandoc.RawInline('latex', '\\end{centering}'))
+  return blocks
 end
 
 -- Function to add the open .ipynb buttons to HTML, you can also add a global method: function Pandoc(doc) { }
@@ -103,49 +81,22 @@ local function post_action_buttons(doc)
   local siteUrl = read_metadata(quarto.project.directory .. '/_quarto.yml')['website']['site-url']
   local output_filepath_without_ext = 
     ternary(is_prod, pandoc.utils.stringify(siteUrl), '') .. get_output_filename_without_ext(doc)
-  quarto.log.debug("output_filepath_without_ext", output_filepath_without_ext)
   local pdf_output_uri = output_filepath_without_ext .. '.pdf'
-  local pdf_link_html = ternary(
-    siteUrl,
-    create_PDF_link(
-      pdf_output_uri, 
-      'Download as PDF', 
-      '/images/badges/png/pdf.png'),
-    '')
-  local links_html = buttons_wrapper(pdf_link_html)
+  local pdf_markdown = create_pdf_markdown(pdf_output_uri)
+  local links_blocks = create_link_wrapper(pdf_markdown)
   local treat_as_qmd = tobool(doc.meta['treat_as_qmd'])
   if str_ends_with(input_file, ".ipynb") and not treat_as_qmd then
     local ipynb_output_uri = output_filepath_without_ext .. '.output.ipynb'
-    local colab_link_html = create_colab_link(
-      ipynb_output_uri,
-      'Open in Colab', 
-      '/images/badges/png/colab.png')
-    local download_link_html = create_download_link(
-      ipynb_output_uri,
-      'Download Notebook', 
-      '/images/badges/png/download-ipynb.png')
-    local deepnote_link_html = create_deepnote_link(
-      ipynb_output_uri,
-      'Open in Deepnote', 
-      '/images/badges/png/deepnote.png')
+    local colab_markdown = create_colab_markdown(ipynb_output_uri)
+    local download_markdown = create_download_markdown(ipynb_output_uri)
+    local deepnote_markdown = create_deepnote_markdown(ipynb_output_uri)
     
-    links_html = buttons_wrapper(
-      colab_link_html .. deepnote_link_html .. download_link_html .. pdf_link_html)
+    links_blocks = create_link_wrapper(
+      colab_markdown .. deepnote_markdown .. download_markdown .. pdf_markdown)
   end
 
   local body_blocks = pandoc.List:new{}
-  if quarto.doc.is_format('pdf') then
-    -- this is the same as `quarto pandoc index.html -o index.pdf`
-    local html_blocks = pandoc.read(links_html, 'html').blocks
-    body_blocks:insert(pandoc.RawInline('latex', '\\begin{centering}'))
-    body_blocks:extend(html_blocks)
-    body_blocks:insert(pandoc.RawInline('latex', '\\end{centering}'))
-  elseif quarto.doc.is_format('html') then
-    -- this just parses the raw HTML and returns pandoc.RawBlock and pandoc.RawInline, 
-    --- which is not convertable to PDF, see: https://quarto.org/docs/visual-editor/technical.html#latex-and-html
-    local html_blocks = quarto.utils.string_to_blocks(links_html)
-    body_blocks:extend(html_blocks)
-  end
+  body_blocks:extend(links_blocks)
   body_blocks:extend(doc.blocks)
 
   local new_doc = pandoc.Pandoc(body_blocks, doc.meta)
