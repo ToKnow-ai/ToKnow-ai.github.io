4c16cc5d2da67903796d8473e3570a5b47a611c5
Kabue Charles <mckabue@gmail.com>
2024-10-06 18:30:19 +0300

    Add image attributes handling and resize options for web screenshots

--- python_utils/web_screenshot.py
+++ python_utils/web_screenshot.py
@@ -14,7 +14,8 @@ async def web_screenshot_async(
         height: int = 0,
         # pyppeteer.page.Page.screenshot options
         screenshot_options: dict = {'fullPage': True },
-        crop_options: dict[Literal['left', 'top', 'right', 'bottom'], int | None] | None = None) -> Image.Image:
+        crop_options: dict[Literal['left', 'top', 'right', 'bottom'], int | None] | None = None,
+        resize_options: dict[Literal['size', 'quality', 'optimize'], int | None] | None = None,) -> Image.Image:
     
     page, browser = await get_browser_page_async(executable_path, width = width, height = height)
     
@@ -39,4 +40,25 @@ async def web_screenshot_async(
         crop_options.get('right', image.size[0]), 
         crop_options.get('bottom', image.size[1]))
     image = image.crop(crop_cordinates)
+    # RESIZE THE IMAGE
+    resize_options_size = (resize_options or {}).get('size', 1)
+    if resize_options_size < 1:
+        # Resize the image
+        resized_img = image.resize(
+            (int(image.size[0] * resize_options_size), 
+             int(image.size[1] * resize_options_size)))
+        # Create a byte stream
+        byte_stream = io.BytesIO()
+        # Save the image to the byte stream with optimization
+        resize_options_optimize = (resize_options or {}).get('optimize', True)
+        resize_options_quality = (resize_options or {}).get('quality', 85)
+        resized_img.save(
+            byte_stream, 
+            format='JPEG', 
+            optimize=resize_options_optimize, 
+            quality=resize_options_quality)
+        # Move to the start of the stream
+        byte_stream.seek(0)
+        # Reopen the optimized image from the byte stream
+        image = Image.open(byte_stream)
     return image
